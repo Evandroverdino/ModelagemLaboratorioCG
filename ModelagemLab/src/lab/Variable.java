579a56d779d31184432cdbb9628676c6c58ebf30
Evandro Verdino <evandroverdino@gmail.com>
2016-09-14 15:01:32 -0300

    Initial Commit
    Upload para o repositório

---
+++ ModelagemLab/src/lab/Variable.java
@@ -0,0 +1,14 @@
+package lab;
+
+public class Variable {
+
+	public static float obs[] = { 0.0f, 1.0f, 0.0f };
+	public static float look[] = { 0.0f, 1.0f, 0.0f };
+	public static float z[] = {0.0f,1.0f,0.0f};
+	public static float tetaxz = 5;
+	public static float raioxz = 0.1f;
+
+	
+	
+	
+}
